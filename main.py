ff567d6418ceb9452d5919a20e7627c3997e9dd1
sebasz-a <jpabuena@unal.edu.co>
2023-08-21 23:24:53 -0500

    Prueba1

---
+++ main.py
@@ -0,0 +1,46 @@
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+
+    def cantidadAsientos(self):
+        n = 0  #contador de objetos Asiento en la lista asientos
+        for i in self.asientos:
+            if i != None:
+                n+= 1
+        return n
+    
+    def verificarIntegridad(self):
+        if self.registro == self.motor.registro:
+            for i in self.asientos:
+                if i.registro != self.registro:
+                    return "Las piezas no son originales"
+        return "Auto original"
+        
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        if color in ["rojo", "negro", "amarillo", "verde", "blanco", "negro"]:
+            self.color = color
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if tipo in ["electrico", "gasolina"]:
+            self.tipo = tipo
